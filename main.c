50f19b8d6c484c3a2fd2dba74c88c638fb8f471a
Shrrek <85354673+Shrrek@users.noreply.github.com>
2023-02-20 19:12:54 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jperales <jperales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 17:29:00 by jperales          #+#    #+#             */
-/*   Updated: 2023/02/20 18:57:27 by jperales         ###   ########.fr       */
+/*   Updated: 2023/02/20 19:05:46 by jperales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 
 int main(int argc, char **argv, char **env)
 {
-//	t_general   *var;
+	t_general   var;
 	int	i;
 
 	(void)argc;
 	(void)argv;
 
 	i = 0;
-	while (env)
+	
+	while (env[i])
 	{
-		printf("%s \n", env[i]);
+		printf("Aqui %d\n", i);
+		var.env[i] = env[i];
+		printf("%s \n", var.env[i]);
 		i++;
 	}
 	return (0);
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: jperales <jperales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 17:16:27 by jperales          #+#    #+#             */
-/*   Updated: 2023/02/20 18:49:12 by jperales         ###   ########.fr       */
+/*   Updated: 2023/02/20 19:01:06 by jperales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 typedef struct s_general
 {
+	char	**env;
 	char    *user;
 	char    *path;
 	char    *home;
